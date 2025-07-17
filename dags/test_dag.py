c14111915d9b5f33c1567e52be435cd8acca49ec
ryan(SON) <ryanp3@naver.com>
2025-07-17 16:50:34 +0900

    test_dag DAG로 인식하는지 확인

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,8 +1,26 @@
-print('hello')
-print('world')
-print('i in ec2 private!')
-print('i failed 1')
-print('i failed 2')
-print('i failed 3')
-print('i failed 4')
-print('i in ec2 private!')
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.utils.task_group import TaskGroup
+from datetime import timedelta
+
+from airflow.models import Variable
+
+from airflow.utils.dates import datetime
+
+def push_message_to_xcom(**kwargs):
+    return 'im in private ec2'
+
+with DAG(
+    dag_id='test_dag',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['test', 'private_ec2'],
+) as dag:
+    simple_task = PythonOperator(
+        task_id='push_message',
+        python_callable=push_message_to_xcom,
+        provide_context=True,
+    )
+    simple_task
