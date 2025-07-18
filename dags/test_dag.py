cb00db773d918a443ed1220aade0a603ef1eb4a9
ryan(SON) <ryanp3@naver.com>
2025-07-18 15:49:16 +0900

    Update test_dag.py

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -9,7 +9,7 @@ from airflow.models import Variable
 from airflow.utils.dates import datetime
 
 def push_message_to_xcom(**kwargs):
-    return 'testing actions step by step 7'
+    return 'testing actions step by step 8'
 
 with DAG(
     dag_id='test_dag',
