da15c74ba2f79b782ad4b5b32836243ced9ac539
jinyoung0711 <ssconvergence2@gmail.com>
2025-07-23 13:34:31 +0900

    refactor: WEATHER_TO_TAGS_MAP 등 상수를 constants.py로 분리

---
+++ api/constants.py
@@ -0,0 +1,48 @@
+# 날씨 조건 -> 태그 매핑
+WEATHER_TO_TAGS_MAP = {
+    "Clear": [
+        "happy", "love", "summer", "sunny", "upbeat", "pop", "dance", "fun",
+        "pop punk", "twerk anthem", "teen pop", "happy dance", "love anthem",
+        "party music", "catchy as fuck", "dance-pop", "pop rock", "pop-rap",
+        "sunshine pop", "reggaeton", "dancehall", "summer hits", "banger",
+        "samba", "party", "uplifting", "pop perfection", "disco pop",
+        "danceable", "good vibes"
+    ],
+    "Cloudy": [
+        "indie", "alternative", "mellow", "chill", "folk", "soft rock",
+        "indie pop", "indie folk", "indie rock", "chillwave", "dream pop",
+        "post-rock", "singer-songwriter", "jangle pop", "folk rock",
+        "alternative dance", "twee pop", "chillout", "cloudy pop", "surf rock"
+    ],
+    "Rainy": [
+        "sad song", "heartbreak", "ballad", "melancholic", "crying my eyes out",
+        "tear-jerker", "bittersweet", "heartbreakingly beautiful", "sadcore",
+        "sad girl", "songs to cry to", "breakup", "regret", "despondency",
+        "longing", "yearning", "cryingggg"
+    ],
+    "Stormy": [
+        "metal", "hardcore", "emo", "punk", "aggressive", "screamo", "deathcore",
+        "death metal", "black metal", "heavy metal", "thrash metal",
+        "grindcore", "nu-metal", "noise rock", "sludge metal", "hardcore punk",
+        "dark plugg", "violent"
+    ],
+    "Snowy": [
+        "christmas", "winter", "dreamy", "ethereal", "ambient pop", "dreamy pop",
+        "snowy", "icy"
+    ],
+    "Windy": [
+        "jazz", "instrumental", "smooth", "airy", "ambient", "jazz fusion",
+        "smooth jazz", "ambient dub", "jazz rap", "acid jazz", "bossa nova",
+        "instrumental hip hop", "lounge", "easy listening", "cinematic",
+        "chill jazz", "chamber jazz", "piano jazz", "flute", "acoustic instrumental"
+    ],
+    "Hot": [
+        "tropical house", "reggaeton", "baile funk", "dembow", "summer hits",
+        "samba rock", "samba de raiz", "latin pop", "dancehall", "moombahton",
+        "tropical", "caliente", "hot", "afrobeat", "afropiano"
+    ],
+    "Cold": [
+        "dark pop", "coldwave", "gothic", "darkwave", "industrial", "icy",
+        "dark ambient", "witch house", "blackgaze", "depressive black metal"
+    ],
+}
