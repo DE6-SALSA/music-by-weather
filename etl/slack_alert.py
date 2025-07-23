40e4a27eea1c59cf1f541d9e6221a836123c8c31
hee <44639251+sunrise412@users.noreply.github.com>
2025-07-23 10:43:45 +0900

    fix: slack_alert.py
    fix webhook url

--- etl/slack_alert.py
+++ etl/slack_alert.py
@@ -9,7 +9,6 @@ def slack_alert(context):
     execution_date = context.get('execution_date')
     log_url = context.get('task_instance').log_url
     webhook_url = Variable.get("SLACK_KEY")
-    # webhook_url = "https://hooks.slack.com/services/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"
 
     message = {
         "text": (
